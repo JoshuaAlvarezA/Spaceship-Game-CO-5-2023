f77f5a8270702081dcc3572578899015525540ed
JoshuaAlvarezA <136272174+JoshuaAlvarezA@users.noreply.github.com>
2023-06-12 20:15:02 -0500

    Clase 1 update
    Revision del codigo y funcionando correctamente. Nave se desplaza como fue indicado en clase

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -4,7 +4,7 @@ from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
 class Spaceship:    #para agregar imagen necesitamos posicion
     X_POS = (SCREEN_WIDTH // 2) - 40
     Y_POS = 500 
-#con self se vuelve atributo 
+#con self se vuelve atributo, python lo convierte automaticamente 
 
     def __init__(self):
         self.image = SPACESHIP 
@@ -17,8 +17,11 @@ class Spaceship:    #para agregar imagen necesitamos posicion
         #se debe mover solo si se oprime tecla
         if user_input [pygame.K_LEFT]:
             self.move_left()
+        elif user_input [pygame.K_DOWN]:
             self.move_down()
+        elif user_input [pygame.K_RIGHT]:
             self.move_right()
+        elif user_input [pygame.K_UP]:
             self.move_up()
 
     
@@ -44,13 +47,13 @@ class Spaceship:    #para agregar imagen necesitamos posicion
 
     def move_up(self):
         #restriccion para que no salga de los limites 
-        if self.rect.y < SCREEN_HEIGHT // 2:
+        if self.rect.y > SCREEN_HEIGHT // 2:
             self.rect.y -= 10 
     
 
     
     def move_down(self):
         #restriccion para que no salga de los limites 
-        if self.rect.y > SCREEN_HEIGHT:
+        if self.rect.y < SCREEN_HEIGHT - 60:
             self.rect.y += 10 
     
